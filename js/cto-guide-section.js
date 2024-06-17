614d14a02b4ba79ea7c903e087c4824e57cdf695
Rick Graham <dynokicks@gmail.com>
2024-06-17 17:46:27 -0400

    Update site

---
+++ js/cto-guide-section.js
@@ -0,0 +1,12 @@
+          <div class="row my-5">
+            <div class="col-12">
+              <h2>CTO Guide</h2>
+
+              <div class="row my-5 justify-content-center">
+                <div class="col-12 col-md-6 col-lg-4 col-xl-3">
+                  <cto-guide-card>
+                </div>
+              </div>
+            </div>
+          </div>
+          
